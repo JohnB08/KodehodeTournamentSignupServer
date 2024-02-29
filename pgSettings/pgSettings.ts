772133e476c4bb63a5ca335e65f430e9e6a58829
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-29 15:01:11 +0000

    added propper env to server

--- pgSettings/pgSettings.ts
+++ pgSettings/pgSettings.ts
@@ -3,9 +3,9 @@ const {Pool} = pkg
 
 
 export const db = new Pool({
-    user: "John",
-    password: "ein2tre4",
-    host: "postGres",
-    database: "bracketsDB",
-    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME
 })
